b094670b8dc0ef18774ce9cfdc91b25b043d1dfc
Sseergey <work4read@yandex.ru>
2021-09-18 19:57:34 +0300

    Create test_ada4254.ino

---
+++ test_ada4254.ino
@@ -0,0 +1,28 @@
+#include "ada4254.h"
+
+const int slaveSelectPin = 4;
+
+ADA4254 ada(&SPI, 4, 0);
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(slaveSelectPin, OUTPUT);
+  digitalWrite(slaveSelectPin, levelCS1);
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(14000, MSBFIRST, SPI_MODE0));
+
+  while (!ada.available()) {
+    ;
+  }
+}
+
+
+void loop()
+{
+    ada.init();
+    ada.calibration();
+    ada.connectInputA();
+    ada.setGain(GAIN1);
+}
